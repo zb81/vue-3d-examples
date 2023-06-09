052f9d8d0e5cdfa6742b995242f1c7f9f136b1f6
ZhuBei <zbneversettle@gmail.com>
2023-06-10 00:20:17 +0800

    use hash mode, fix 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const Home = () => import('@/views/home/Home.vue')
 const City = () => import('@/views/city/City.vue')
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
